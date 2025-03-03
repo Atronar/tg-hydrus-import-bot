80b1b7e2a47f765b33d06aa3eb866d376057b5ae
Atronar <atronar@users.noreply.github.com>
2025-03-03 18:18:09 +0300

    Причёсан вид модуля конфига

--- tg-hydrus-import-bot/config.py
+++ tg-hydrus-import-bot/config.py
@@ -10,18 +10,41 @@ import shutil
 from pydantic import BaseModel, StringConstraints, Field, ValidationError
 
 class ConfigModel(BaseModel):
-    LOG_LEVEL: str|int
+    LOG_LEVEL: int|Literal["TRACE","DEBUG","INFO","SUCCESS","WARNING","ERROR","CRITICAL"]
     LOG_PATH: str
     TG_ADMIN_ID: list[Annotated[int, Field(ge=0)]]
-    TG_BOT_TOKEN: Annotated[str, StringConstraints(strip_whitespace=True, pattern=r'^\d+:\S{35}$')]
-    HYDRUS_TOKEN: Annotated[str, StringConstraints(strip_whitespace=True, min_length=64, max_length=64)]
+    TG_BOT_TOKEN: Annotated[
+        str, 
+        StringConstraints(strip_whitespace=True, pattern=r'^\d+:\S{35}$')
+    ]
+    HYDRUS_TOKEN: Annotated[
+        str,
+        StringConstraints(
+            strip_whitespace=True,
+            min_length=64,
+            max_length=64,
+            pattern=r'^[\da-f]*$'
+        )
+    ]
     HYDRUS_API_URL: str
     TIME_TO_SLEEP: Annotated[float, Field(gt=0)]
     TAGS_NAMESPACE: str
     DESTINATION_PAGE_NAME: str
     TEMP_PATH: str
-    CONTENT_TYPES: list[Literal[ "photo", "video", "animation", "video_note", "audio", "voice", "document", "text" ]]
-    SAUCENAO_TOKEN: str
+    CONTENT_TYPES: list[Literal[
+        "photo",
+        "video",
+        "animation",
+        "video_note",
+        "audio",
+        "voice",
+        "document",
+        "text"
+    ]]
+    SAUCENAO_TOKEN: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True, pattern=r'^[\da-f]*$')
+    ] = ""
 
 __CONFPATH = ".conf.json"
 """Local path to json config"""
