989f0af6f6be5182b612e0e2f86b3803e3e7d8c7
Atronar <atronar@users.noreply.github.com>
2025-03-03 19:04:42 +0300

    Оптимизирован код human_bytes

--- tg-hydrus-import-bot/human_bytes.py
+++ tg-hydrus-import-bot/human_bytes.py
@@ -7,8 +7,10 @@ class HumanBytes:
     """Предполагается использовать единственную функцию format
     Константы позволяют модифицировать класс через создание дочерних
     """
-    METRIC_LABELS: tuple[str, ...] = ("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    BINARY_LABELS: tuple[str, ...] = ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+    __slots__ = ()  # Запрещаем создание атрибутов экземпляра
+
+    METRIC_LABELS: tuple[str, ...] = ("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB", "RB", "QB")
+    BINARY_LABELS: tuple[str, ...] = ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB", "RiB", "QiB")
     PRECISION_OFFSETS: tuple[float, ...] = (0.5, 0.05, 0.005, 0.0005) # PREDEFINED FOR SPEED.
     PRECISION_FORMATS: tuple[str, ...] = (
         "{}{:.0f} {}",
@@ -24,34 +26,42 @@ class HumanBytes:
         or metric (powers of 1000) representation.
         """
 
-        assert isinstance(num, (int, float)), "num must be an int or float"
-        assert isinstance(metric, bool), "metric must be a bool"
-        assert isinstance(precision, int) and 0 <= precision <= 3, "precision must be an int (range 0-3)"
+        # assert, ранее бывший здесь, отключается флагом -O в Python, что может привести к ошибкам
+        if not isinstance(num, (int, float)):
+            raise TypeError("num must be an int or float")
+        if not isinstance(metric, bool):
+            raise TypeError("metric must be a bool")
+        if not isinstance(precision, int) or precision < 0:
+            raise ValueError("precision must be a non-negative integer")
 
         unit_labels = HumanBytes.METRIC_LABELS if metric else HumanBytes.BINARY_LABELS
-        last_label = unit_labels[-1]
+        max_index = len(unit_labels) - 1
         unit_step = 1000 if metric else 1024
-        unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
 
-        is_negative = num < 0
-        if is_negative: # Faster than ternary assignment or always running abs().
-            num = abs(num)
+        # VERY IMPORTANT:
+        # Only accepts the CURRENT unit if we're BELOW the threshold where
+        # float rounding behavior would place us into the NEXT unit: F.ex.
+        # when rounding a float to 1 decimal, any number ">= 1023.95" will
+        # be rounded to "1024.0". Obviously we don't want ugly output such
+        # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
+        if precision > 3:
+            precision_format = "{}{:.%df} {}" % precision
+            unit_step_thresh = unit_step - 0.5 / (10 ** precision)
+        else:
+            precision_format = HumanBytes.PRECISION_FORMATS[precision]
+            unit_step_thresh = unit_step - HumanBytes.PRECISION_OFFSETS[precision]
 
-        unit = unit_labels[0]
-        for unit in unit_labels:
-            if num < unit_step_thresh:
-                # VERY IMPORTANT:
-                # Only accepts the CURRENT unit if we're BELOW the threshold where
-                # float rounding behavior would place us into the NEXT unit: F.ex.
-                # when rounding a float to 1 decimal, any number ">= 1023.95" will
-                # be rounded to "1024.0". Obviously we don't want ugly output such
-                # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
-                break
-            if unit != last_label:
-                # We only shrink the number if we HAVEN'T reached the last unit.
-                # NOTE: These looped divisions accumulate floating point rounding
-                # errors, but each new division pushes the rounding errors further
-                # and further down in the decimals, so it doesn't matter at all.
-                num /= unit_step
+        sign = "-" if num < 0 else ""
+        if sign: # Faster than ternary assignment or always running abs().
+            num = -num
 
-        return HumanBytes.PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
+        unit_index = 0
+        while unit_index < max_index and num >= unit_step_thresh:
+            # We only shrink the number if we HAVEN'T reached the last unit.
+            # NOTE: These looped divisions accumulate floating point rounding
+            # errors, but each new division pushes the rounding errors further
+            # and further down in the decimals, so it doesn't matter at all.
+            num /= unit_step
+            unit_index += 1
+
+        return precision_format.format(sign, num, unit_labels[unit_index])
