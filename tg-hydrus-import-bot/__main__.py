9599e81d32ac96f6ff37786cfb31a870c8bf7bd2
Atronar <atronar@users.noreply.github.com>
2025-03-03 05:38:08 +0300

    Добавлена обработка отсутствующего подключения при незапущенном Гидрусе

--- tg-hydrus-import-bot/__main__.py
+++ tg-hydrus-import-bot/__main__.py
@@ -9,6 +9,7 @@ import urllib.parse
 
 import hydrus_api
 from loguru import logger
+import urllib3.exceptions
 
 from start_script import start_script
 from tools import prepare_temp_folder
@@ -38,9 +39,27 @@ def main():
     except hydrus_api.DatabaseLocked as ex:
         if ex.response.status_code==503:
             parsed_url = urllib.parse.urlparse(ex.response.url)
+            connection_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
             logger.info("Не удалось подключиться к Hydrus. "
-                        f"Адрес подключения {parsed_url.scheme}://{parsed_url.netloc}")
+                        f"Адрес подключения {connection_url}")
             logger.debug(f"{ex.response.reason}: {ex.response.url}")
+            logger.info(f"Программа приостановлена на {CONF.TIME_TO_SLEEP} сек.")
+            time.sleep(CONF.TIME_TO_SLEEP)
+        else:
+            raise ex
+    except hydrus_api.ConnectionError as ex:
+        if ex.args and isinstance(ex.args[0], urllib3.exceptions.MaxRetryError):
+            ex = ex.args[0]
+                
+            parsed_url = urllib.parse.urlparse(ex.url)
+            connection_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+            if isinstance(ex.reason, urllib3.exceptions.NewConnectionError):
+                connection_url = f"{ex.reason.conn.host}:{ex.reason.conn.port}"
+
+            logger.info("Не удалось подключиться к Hydrus. "
+                        f"Адрес подключения {connection_url}")
+            logger.debug(f"{ex.reason}: {ex.url}")
             logger.info(f"Программа приостановлена на {CONF.TIME_TO_SLEEP} сек.")
             time.sleep(CONF.TIME_TO_SLEEP)
         else:
